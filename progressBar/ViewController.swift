e03ceaffe1e203253a372b0d78be48bd79378935
Manthan Kevadiya <manthankevadiya11@gmail.com>
2023-03-17 12:42:42 +0530

    Mathan

--- progressBar/ViewController.swift
+++ progressBar/ViewController.swift
@@ -9,9 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var progressBar: UIProgressView!
+    var time = Timer()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    @IBAction func startButtonAction(_ sender: Any) {
+        
+        var randomTime : Float = 0.0
+        self.progressBar.progress = randomTime
+        time = Timer.scheduledTimer(withTimeInterval: 0.05, repeats: true, block: { (_) in
+            randomTime += 0.05
+            self.progressBar.progress = randomTime
+            if self.progressBar.progress == 1.0{
+                
+                self.time.invalidate()
+               
+            }
+        })
+       
     }
 
 
